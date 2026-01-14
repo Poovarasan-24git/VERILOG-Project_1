e65c69e2c7cba53efcd061e2f0b0502d9c6bec71
Poovarasan-24git <Poovarasan24011@gmail.com>
2026-01-14 15:35:09 +0530

    even_odd.v

--- src/even_odd.v
+++ src/even_odd.v
@@ -5,10 +5,10 @@ module design_even_odd   ( input a,input b,input c,input x,     //x=0 for even p
     assign parity_bit = a^b^c;  
     assign parity_gen = a^b^c^parity_bit;
 always @(*) begin      
-    //even parity                                        //Execute this block whenever ANY input used inside this block changes
-    if( 1'b0 == x ) begin                   //decides whether it is even parity or odd parity
-         parity_ch1= parity_gen^0;          //parity checker 1
-         parity_ch2= parity_gen^1;          //parity checker 2
+    //even parity                     //Execute this block whenever ANY input used inside this block changes
+    if( 1'b0 == x ) begin             //decides whether it is even parity or odd parity
+         parity_ch1= parity_gen^0;    //parity checker 1
+         parity_ch2= parity_gen^1;    //parity checker 2
     end
     else begin
     //odd parity
